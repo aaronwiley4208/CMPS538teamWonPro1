db53dfcdd29e2593f0a950c59f3e647de89a8989
Jason VR Rig\Jason Woodworth <DownToTheLastPixel@gmail.com>
2019-02-05 00:21:33 -0600

    Added a basic pellet sizeup system with a basic map.
    Right now pellets check the size of pacman on collision and are chomped if they're smaller than him. Chomping checks if he should grow, and ups the requirement to grow by one. Later, the requirement should be a size req.

---
+++ pacman-source/Assets/Systems/PacMan/PacManSize.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls the size of pacman, with functions for adding pellets and stuff.
+/// Size goes up in small increments every so many pellets. 
+/// </summary>
+public class PacManSize : MonoBehaviour {
+
+    public float currentSize;
+    public float pelletsTilSizeUp;
+
+    private float currentPelletCount = 0;
+    private PacManSize instance;
+
+	// Use this for initialization
+	void Awake () {
+        if (instance == null) instance = this;
+        else Destroy(this);
+
+        currentSize = transform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // When the character hits something, react if its a pellet
+    void OnControllerColliderHit(ControllerColliderHit hit) {
+        Debug.Log(hit.gameObject.name);
+        if (hit.gameObject.tag == "Pellet")
+            Debug.Log("Yo Pellet");
+    }
+
+    /// <summary>
+    /// Consume a pellet of given size.
+    /// Add one to our pellet count and check if we need to size up.
+    /// </summary>
+    /// <param name="size"></param>
+    public void Chomp(float size) {
+        currentPelletCount++;
+        if (currentPelletCount >= pelletsTilSizeUp)
+            SizeUp();
+    }
+
+    // Increase PacMan Size
+    private void SizeUp() {
+        currentSize += .2f;
+        transform.localScale = new Vector3(currentSize, currentSize, currentSize);
+        pelletsTilSizeUp++;
+        currentPelletCount = 0;
+    }
+}
---
+++ pacman-source/Assets/Systems/PacMan/Pellet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pellet : MonoBehaviour {
+
+    private float size; // Radius of this pellet.
+
+	// Use this for initialization
+	void Start () {
+        size = transform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    // If PacMan collides with me.
+    void OnTriggerEnter(Collider collider) {
+        if (collider.tag == "PacMan") {
+            // We want to compare our size to pac man's, and let him eat us if we're smaller
+            PacManSize pacSize = collider.GetComponent<PacManSize>();
+            if (pacSize.currentSize >= size) {
+                pacSize.Chomp(size);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
